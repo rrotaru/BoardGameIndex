3338fa346e9de38212162634d999e69551172978
Robert Rotaru <rrotaru@wesleyan.edu>
2015-11-19 15:04:29 -0500

    Working urlValidation

--- src/java/validators/urlValidator.java
+++ src/java/validators/urlValidator.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package edu.ccsu.validators;
+package validators;
 
 import javax.faces.application.FacesMessage;
 import javax.faces.component.UIComponent;
@@ -14,7 +14,7 @@ import javax.faces.validator.ValidatorException;
 
 /**
  *
- * @author cw1491
+ * @author rrotaru
  */
 @FacesValidator(value="urlValidator")
 public class urlValidator implements Validator{
@@ -23,7 +23,7 @@ public class urlValidator implements Validator{
     public void validate(FacesContext context, UIComponent component, Object value) throws ValidatorException {
         String url = (String)value;
         HtmlInputText htmlInputText = (HtmlInputText)component;
-        if (!url.matches("/((([A-Za-z]{3,9}:(?:\\/\\/)?)(?:[-;:&=\\+\\$,\\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\\+\\$,\\w]+@)[A-Za-z0-9.-]+)((?:\\/[\\+~%\\/.\\w-_]*)?\\??(?:[-\\+=&;%@.\\w_]*)#?(?:[\\w]*))?)/")){
+        if (!url.matches("^(https?|ftp|file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]")){
             FacesMessage facesMessage =  new FacesMessage(htmlInputText.getLabel()+": url format is not valid");
             throw new ValidatorException(facesMessage);
         }
